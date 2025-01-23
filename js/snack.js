0539e6a2e287eb44ff2f914316a1944df3a4dd76
FedeRacedo <fefe.racedo99@gmail.com>
2025-01-22 22:30:32 -0300

    Precios, actualizacion de carrito

--- js/snack.js
+++ js/snack.js
@@ -83,11 +83,13 @@ let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
 // Agregar producto al carrito
 function add(nombre, precio) {
+    precio = parseFloat(precio); // Asegúrate de que el precio sea un número
     const productoExistente = carrito.find(item => item.nombre === nombre);
+
     if (productoExistente) {
         productoExistente.cantidad++;
     } else {
-        carrito.push({ nombre, precio: parseFloat(precio.replace('.', '').replace(',', '.')), cantidad: 1 });
+        carrito.push({ nombre, precio, cantidad: 1 });
     }
     actualizarCarrito();
 }
